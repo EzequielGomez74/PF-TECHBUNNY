a91e4df773e6910289937049f0c47d90aeb8942c
Matyarg <matias.g15@hotmail.com>
2022-12-14 14:38:07 -0300

    models creados

--- api/apiServices/users/model.js
+++ api/apiServices/users/model.js
@@ -2,27 +2,57 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "Activity",
+    "User",
     {
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
       name: {
         type: DataTypes.STRING,
-        allownull: false,
+        allowNull: false,
       },
-      difficulty: {
-        type: DataTypes.INTEGER,
-        validate: {
-          min: 1,
-          max: 5,
-        },
-        allownull: false,
+      surname: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      duration: {
-        type: DataTypes.INTEGER,
-        allownull: false,
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      season: {
-        type: DataTypes.ENUM("Summer", "Autumn", "Spring", "Winter"),
-        allownull: false,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      billingAddress: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "",
+      },
+      defaultShippingAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM(["admin","user"]),
+        allowNull: true,
+        defaultValue: "user",
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultvalue: false,
+      },
+      needPasswordReset: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultvalue: false,
+      },
+      profilePicture: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        defaultvalue: "https://images.pagina12.com.ar/styles/focal_3_2_470x313/public/2022-10/663621-47230-hasbulla_0.png",
       },
     },
     {
