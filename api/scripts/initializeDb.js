b6a6d0637b2ab950306c54d35ed0124f860345e7
Matyarg <matias.g15@hotmail.com>
2022-12-14 17:58:59 -0300

    armando db y sequelize

--- api/scripts/initializeDb.js
+++ api/scripts/initializeDb.js
@@ -1,11 +1,11 @@
 // "esta va a cargar jsons a la data base"
 
 const brands = require("../services/db/assets/brands.json");
-const category = require("../services/db/assets/category.json");
-const subcategory = require("../services/db/assets/subcategory.json");
+const categories = require("../services/db/assets/categories.json");
+const subcategories = require("../services/db/assets/subcategories.json");
 const { Category, SubCategory } = require("../services/db/db.js")
 
-async function loadtodb(array, model) {
+async function loadtoDb(array, model) {
     try {
        await model.bulkCreate(array)
     } catch (error) {
@@ -13,9 +13,7 @@ async function loadtodb(array, model) {
     }
 }
 
-loadtodb(category, Category)
-
 function loadAllAssets() {
-    loadtodb(category, Category);
-    loadtodb(subcategory, SubCategory);
+    loadtoDb(categories, Category);
+    loadtoDb(subcategories, SubCategory);
 }
