3899be930e0f2360ee6c923c446ac459a56cd46b
Matyarg <matias.g15@hotmail.com>
2022-12-15 04:10:16 -0300

    gg db fixed

--- api/apiServices/category/model.js
+++ api/apiServices/category/model.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
+        autoIncrement: true,
       },
       name: {
         type: DataTypes.STRING,
