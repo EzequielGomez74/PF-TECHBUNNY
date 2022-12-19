526832310262a7de97c435fd009e8ab73cbd719c
Alberto Martinez <srbetito85@gmail.com>
2022-12-19 16:44:29 -0300

    roto3

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const router = Router();
+const auth = require("../apiServices/login/routes.js");
 const register = require("../apiServices/register/routes.js");
 const products = require("../apiServices/product/routes.js");
+const enter = require("../apiServices/enter/routes.js");
 
 //router.use("/register",register);
+//router.use("/auth",login)
+
+//router.use("/enter",enter)
 router.use("/products", products);
 
 module.exports = router;
