4aced92670b1272632f19bb938d093b4698c76ff
Alissa Gamarra <alissa.gamarra@gmail.com>
2022-12-20 16:27:39 -0500

    detalles

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,4 @@
-import { GET_ALL_PRODUCTS, FILTER_BY_CATEGORY } from './actionTypes'
+import { GET_ALL_PRODUCTS,  } from './actionTypes'
 
 export const getProducts = () => {
     return function(dispatch){
@@ -9,9 +9,37 @@ export const getProducts = () => {
     }
 }
 
-export const filterByCategory = (products, idCategory) => {
-    return function(dispatch){
-        const filtered = products.filter(p => p.category === idCategory)
-        dispatch({type:FILTER_BY_CATEGORY, payload: filtered})
-    }
-}
+// export const productsByCategory = (products) => {
+//     return function(dispatch){
+//         const a = products.filter(p => p.category === 'Equipos armados')
+//         const b = products.filter(p => p.category === 'Consolas')
+//         const c = products.filter(p => p.category === 'Notebooks')
+//         const d = products.filter(p => p.category === 'Gabinetes')
+//         const e = products.filter(p => p.category === 'Fuentes y UPS')
+//         const f = products.filter(p => p.category === 'Motherboards')
+//         const g = products.filter(p => p.category === 'Procesadores')
+//         const h = products.filter(p => p.category === 'Cooling')
+//         const i = products.filter(p => p.category === 'Memorias')
+//         const j = products.filter(p => p.category === 'Almacenamiento')
+//         const k = products.filter(p => p.category === 'Tarjetas de video')
+//         const l = products.filter(p => p.category === 'Monitores y TV')
+//         const m = products.filter(p => p.category === 'Sillas')
+//         const n = products.filter(p => p.category === 'Pendrives')
+//         const o = products.filter(p => p.category === 'Impresoras')
+        
+//         const filtered ={
+//             equiposArmados: a,
+//             consolas: b,
+//             notebooks: c,
+//             gabinetes:d,
+//             fuentesYups:e,
+//             motherboards: f,
+//             procesadores: g,
+//             cooling: h,
+//             memorias:i,
+//             almacenamiento:j,
+//             tarjetasDeVideo: k,
+//             monitoresY
+//         }
+//     }
+// }
