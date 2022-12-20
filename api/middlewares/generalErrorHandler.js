a6aa31c8987bc6b0e32919174388748854dd6cab
Alberto Martinez <srbetito85@gmail.com>
2022-12-20 16:31:55 -0300

    backend andando de nuevo

---
+++ api/middlewares/generalErrorHandler.js
@@ -0,0 +1,9 @@
+function generalErrorHandler(err, req, res, next) {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+}
+
+module.exports = generalErrorHandler;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 const router = Router();
-const auth = require("../apiServices/login/routes.js");
-const register = require("../apiServices/register/routes.js");
 const products = require("../apiServices/product/routes.js");
+const refresh = require("../apiServices/refresh/routes.js");
 const enter = require("../apiServices/enter/routes.js");
+const verifyJWT = require("../middlewares/verifyJWT.js");
 
-//router.use("/register",register);
-//router.use("/auth",login)
-
-//router.use("/enter",enter)
+router.use("/enter", enter);
+router.use("/refresh", refresh);
+router.use(verifyJWT); //middleware de validacion de JWT
 router.use("/products", products);
+
+//router.use
 
 module.exports = router;
