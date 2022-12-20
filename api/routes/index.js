12c0b65158cf59e72a11b78c1f07a8a7e7dc6713
Alberto Martinez <srbetito85@gmail.com>
2022-12-20 18:24:14 -0300

    categories endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,14 @@ const router = Router();
 const products = require("../apiServices/product/routes.js");
 const refresh = require("../apiServices/refresh/routes.js");
 const enter = require("../apiServices/enter/routes.js");
+const categories = require("../apiServices/category/routes.js");
 const verifyJWT = require("../middlewares/verifyJWT.js");
 
 router.use("/enter", enter);
 router.use("/refresh", refresh);
-router.use(verifyJWT); //middleware de validacion de JWT
+//router.use(verifyJWT); //middleware de validacion de JWT
 router.use("/products", products);
+router.use("/categories", categories);
 
 //router.use
 
