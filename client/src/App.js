4dccbd422fa097e359ce2f502d328c6fe90c68e0
abibreno <jenniferabigailbreno@gmail.com>
2022-12-21 12:10:25 -0300

    about us modificacion de img

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
-
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Details from './Components/Details/Details'
+// import CardV from './Components/Card V/CardV';
+// import CardH from './Components/Card H/CardH';
+import Home from './Components/Home/Home';
 import About from './Components/About/About';
 
 
