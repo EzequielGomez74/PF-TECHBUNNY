0006f10f35505ea356732fab6a18adda31b61f63
abibreno <jenniferabigailbreno@gmail.com>
2022-12-21 18:43:49 -0300

    landing page 90%, falta agregar el redireccionamiento a Login

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import Details from './Components/Details/Details'
 // import CardH from './Components/Card H/CardH';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
+import LandingPage from './Components/Landing/LandingPage';
 
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/" component={LandingPage} />
         <Route exact path='/home' component={Home}  />
         <Route exact path='/detail/:id' component={Details}  />
         <Route exact path='/about' component={About}  />
---
+++ client/src/Components/Landing/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
+
+export default function LandingPage() {
+  return (
+    <div className="landing">
+      <div className="prueba">
+      <div className="divTitulo">
+      <Link to="/home">
+       <h1>TECHBUNNY</h1>
+      </Link>
+      </div>
+      <div className="divBtn">
+       <div className="btnLogin">
+        <Link to="/">
+         <h2>Login</h2>
+        </Link>
+       </div>
+       <div className="btnInv">
+        <Link to="/home">
+         <h2>Invitado</h2>
+        </Link>
+       </div>
+      </div>
+      </div>
+    </div>
+  );
+}
