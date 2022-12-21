4065d485499d778f6e3bf0b77a756ef39f4ddfcd
Alissa Gamarra <alissa.gamarra@gmail.com>
2022-12-21 13:01:27 -0500

    css 21.12.2022 de Detail Page

--- client/src/Components/Details/Details.jsx
+++ client/src/Components/Details/Details.jsx
@@ -6,7 +6,8 @@ import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
 import s from './Details.module.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart, faStar, faTruck, faStore, faReceipt} from "@fortawesome/free-solid-svg-icons";
+import {faHeart, faStar, faTruck, faStore} from "@fortawesome/free-solid-svg-icons";
+import Dropdown from '../Dropdown/Dropdown';
 
 function Details() {
   const { id } = useParams()
@@ -19,20 +20,39 @@ function Details() {
 
 
   const [quantity, setQuantity] = useState(0)
+  const [stock, setStock]= useState(product.stock)
   const handlePlus = () =>{
     if(quantity < product.stock){
       setQuantity(quantity+1);
+      setStock(stock-1);
     }
   }
 
   const handleMinus = ()=>{
     if(quantity> 0){
-      setQuantity(quantity-1)
+      setQuantity(quantity-1);
+      setStock(stock+1)
     }
   }
   
-  const [input, setInput] = useState('')
-  const [comments, setComments] = useState([])
+  const description = product.description?.map(ele => {
+    const key = Object.keys(ele)[0];
+    const value = Object.values(ele)[0];
+
+    switch (key) {
+      case 'ul':
+        return (<ul>{value.map(data => {
+          return <li>{data}</li>
+        })}</ul>);
+      case 'p':
+        return <p>{value}</p>            
+      default:
+        return <br />;
+    }
+  })
+
+  // const [input, setInput] = useState('')
+  // const [comments, setComments] = useState([])
 
   // const handleInputChange = (e) => {
   //   setInput(e.target.value)
@@ -44,61 +64,53 @@ function Details() {
   // }
 
   return (
-    <div>
+    <div className={s.detailPage}>
       <NavBar />
-      <section>
-        <div>
-          <FontAwesomeIcon icon={faHeart} />
-          <img src={product.image} alt={product.product_id} />
-        </div>
-        <div>
-          <h2>{product.brand}</h2>
-          <h1>{product.name}</h1>
-          <div>
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
+      <section className={s.productDetails}>
+        <div className={s.block}>
+          <div className={s.productImage}>
+            <FontAwesomeIcon icon={faHeart} className={s.heart} />
+            <img src={product.image} alt={product.product_id} />
           </div>
-          <div>{product.description?.map(ele => {
-            const key = Object.keys(ele)[0];
-            const value = Object.values(ele)[0];
-
-            switch (key) {
-              case 'ul':
-                return (<ul>{value.map(data => {
-                  return <li>{data}</li>
-                })}</ul>);
-              case 'p':
-                return <p>{value}</p>            
-              default:
-                return <br />;
-            }
-          })}</div>
-          <div>
-            <h3>Tipo de Entrega</h3>
-            <input type="radio" id="contactChoice1" name="contact" value="email" />
-            <label><FontAwesomeIcon icon={faTruck} /> &nbsp;&nbsp;Despacho a Domicilio </label>
-
-            <input type="radio" id="contactChoice1" name="contact" value="email" />
-            <label><FontAwesomeIcon icon={faStore} /> &nbsp;&nbsp;Retiro en tienda </label>
-          </div>
-        </div>
-        <form>
-          <span>ID Producto: {product.product_id} </span>
-          <h6>Envío gratis</h6>
-          <hr />
-          <h2>US${product.price}</h2>
-          <div>
+          <div className={s.productInfo}>
             <div>
-              <button onClick={handleMinus} >-</button>{quantity}<button onClick={handlePlus}>+</button>
+              <Dropdown description={description} />
             </div>
-            <p>Stock disponible: {product.stock} </p>
           </div>
-          <h5><FontAwesomeIcon icon={faReceipt} /> &nbsp;&nbsp; Disponible con Mercado Pago</h5>
-          <button type='submit'>Agregar al Carrito</button>
-        </form>
+        </div>
+        <div className={s.productCart} >
+          <span className={s.pId} >ID Producto: {product.product_id} </span>
+          <h2 className={s.pBrand}>{product.brand}</h2>
+          <h1 className={s.pName}>{product.name}</h1>
+
+          <div className={s.pScore}>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <div className={s.delivery}>
+            <h3>Tipo de Entrega</h3>
+            <div>
+              <input type="radio" id="contactChoice1" name="contact" value="email" />
+              <label>&nbsp;&nbsp;<FontAwesomeIcon icon={faTruck} /> &nbsp;&nbsp;Despacho a domicilio </label>
+            </div>
+            <div>
+              <input type="radio" id="contactChoice1" name="contact" value="email" />
+              <label>&nbsp;&nbsp;<FontAwesomeIcon icon={faStore} /> &nbsp;&nbsp;Retiro en tienda </label>
+            </div>
+          </div>
+          <hr />
+          <h2 className={s.price}>US${product.price}</h2>
+          <div className={s.quantity} >
+            <div>
+              <button onClick={handleMinus} >-</button>&nbsp;&nbsp;&nbsp;&nbsp;{quantity}&nbsp;&nbsp;&nbsp;&nbsp;<button onClick={handlePlus}>+</button>
+            </div>
+            <span className={s.stock} >&nbsp;&nbsp;&nbsp;&nbsp;Stock disponible: {stock} </span>
+          </div>
+          <button type='submit' className={s.mainButton}>Agregar al Carrito</button>
+        </div>
       </section>
       <section>
         CARRUSEL
@@ -107,12 +119,12 @@ function Details() {
         <div>
           <div>
             <h5>Calificación general:</h5>
-            <span>{comments.length} comentarios</span> 
+            <span>0 comentarios</span> 
             {/* Esto tiene que ser dinamico */}
           </div>
           <hr />
           {/* investigaa cómo hacer que las estrellas se muestren */}
-          <input type="text" value={input} placeholder='Escribe un comentario sobre tu compra' />
+          <input type="text" placeholder='Escribe un comentario sobre tu compra' />
           <button>Agregar Comentario</button>
         </div>
       </section>
