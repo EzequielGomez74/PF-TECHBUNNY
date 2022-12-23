2a2e1031bcd59a2da8e359160e32b3ce94dd0d9c
Alberto Martinez <srbetito85@gmail.com>
2022-12-23 14:08:31 -0300

    db rara

--- api/apiServices/product/controller.js
+++ api/apiServices/product/controller.js
@@ -89,13 +89,13 @@ async function createProduct(product) {
   }
 }
 
-async function deleteProduct(productId) {
+async function deleteProduct(product_id) {
   try {
     await Product.update(
       { active: false },
       {
         where: {
-          product_id: productId,
+          product_id,
         },
       }
     );
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,18 +7,20 @@ const category = require("../apiServices/category/routes.js");
 const subcategory = require("../apiServices/subcategory/routes.js");
 const brand = require("../apiServices/brand/routes.js");
 const country = require("../apiServices/country/routes.js");
-const verifyJWT = require("../middlewares/verifyJWT.js");
 const review = require("../apiServices/review/routes.js");
+const order = require("../apiServices/order/routes.js");
+const verifyJWT = require("../middlewares/verifyJWT.js");
 
 router.use("/enter", enter);
 router.use("/refresh", refresh);
-router.use(verifyJWT); //middleware de validacion de JWT
+//router.use(verifyJWT); //middleware de validacion de JWT
 router.use("/products", products);
 router.use("/categories", category);
 router.use("/subcategories", subcategory);
 router.use("/brands", brand);
 router.use("/reviews", review);
 router.use("/countries", country);
+router.use("/orders", order);
 
 //router.use
 
