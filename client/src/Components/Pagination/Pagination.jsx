b6fcbc151a5ed275c8fd71f90064c63cce9c2103
EzequielGomez74 <ezequiel.canob@gmail.com>
2022-12-26 18:18:46 -0300

    PAGINATION

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -1,38 +1,34 @@
 import React from "react";
 import './Pagination.css';
 
-export default function Pagination({
-    productsPerPage,
-    allProducts,
-    paginate,
-    currentPage,
-}) {
+export default function Pagination({productsPerPage, products, paginate, currentPage}){
     const pageNumbers = [];
-
-    for (let i = 1; i <= Math.ceil(allProducts/productsPerPage); i++){
-        pageNumbers.push(i);
+  
+    for(let i = 1; i <= Math.ceil(products/productsPerPage); i++){
+      pageNumbers.push(i)
     }
-
-    if(currentPage === pageNumbers.length + 1) {
-        paginate(1);
+    if(currentPage === pageNumbers.length + 1){
+      paginate(1)
     }
-
+  
     return(
-        <div className="pagination">
-            <button className="btnPag" onClick={() => paginate(currentPage === 1 ? pageNumbers.length : currentPage -1)} >
-            «{" "}
+      <div className="btnPag">
+        <button
+          onClick={() => 
+            paginate(currentPage === 1 ? pageNumbers.length : currentPage -1)}
+            > « </button>
+  
+        {
+          pageNumbers && pageNumbers.map(number => (
+            <button key = {number} onClick = {() => paginate(number)}>
+              {currentPage === number ? <b>{number}</b> : number}
             </button>
-            {
-                pageNumbers && pageNumbers.map((number) => (
-                    <button className="btnPag" key={number} onClick={() => paginate(number)}>
-                        {currentPage === number ? <b>{number}</b> : number}
-                    </button>
-                ))
-            }
-            <button className="btnPag" onClick={() => paginate(currentPage === 0 ? currentPage : currentPage + 1)}>
-            »{" "}
-            </button>
-        </div>
+          ))
+        }
+  
+        <button onClick={() => paginate(currentPage === 0 ? currentPage : currentPage + 1)}
+        > » </button>
+      </div>
     )
-
-}
+  };
+  
