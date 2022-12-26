b0f8ded202ab5843fa977c7d8ec49ea986c7d573
Matyarg <matias.g15@hotmail.com>
2022-12-26 12:48:00 -0300

    verificaciones de datos casi listas (bodyvalidators en scripts)

--- api/scripts/bodyValidators/index.js
+++ api/scripts/bodyValidators/index.js
@@ -2,16 +2,26 @@ const { check } = require("express-validator");
 const { validateResult } = require("../validateHelper.js");
 
 const user = [
-  // check("user_id").not().exists().custom({error: "no podes modificar el user id"}),
-  //check("name").not().exists(),
-  //check("role").not().exists(),
-  //check("refreshToken").not().exists().custom({error: "no podes modificar refresh"}),
-  //check('username').exists().not().isEmpty(),
-  check("name").isString(),
-  // check("defaultShippingAddress").exists(),
-  //check("email").isEmail(),
-  //check("password").isString().isLength({ min: 6 }),
-  // check("profilePicture").isURL(),
+  //estos no se deben de modificar
+  check("user_id").not().isEmpty().withMessage("no podes pasar un user id"),
+  check("role").not().exists().withMessage("no podes un role"),
+  check("refreshToken").not().exists().withMessage("no podes modificar el refresh token"),
+  check('isActive').isEmpty().withMessage("no podes modificar isActive"),
+  check('needPasswordReset').isEmpty().withMessage("no podes modificar needPasswordReset"),
+  check('isLogged').isEmpty().withMessage("no podes modificar isLogged"),
+  
+  
+  //estos si se pueden modificar
+  check('username').exists().isString().withMessage("el username debe ser un string"),
+  check("name").exists().isString(),
+  check("password").isString().isLength({ min: 6 }).withMessage("la password debe tener al menos 6 caracteres"),
+  check("email").isEmail(),
+  check("profilePicture").isURL().withMessage("La foto de perfil debe ser una url"),
+  check("defaultShippingAddress").exists().withMessage("no podes modificar isLogged"),
+  check("surname").isString().withMessage("el surname debe ser un string"),
+  check("billingAddress").isString().withMessage("el billingAddress debe ser un string"),
+  check("zipCode").isNumeric().withMessage("el zipCode debe un numero"),
+
   (req, res, next) => {
     validateResult(req, res, next);
   },
@@ -50,13 +60,13 @@ const order = [
 
 module.exports = { user, product, order };
 
-// user_id: {
-// username: {
-// name: {
-// surname: {
-// password: {
+// user_id: 
+// username: 
+// name: 
+// surname: 
+// password: 
 // email:
-// billingAddress: {
+// billingAddress: 
 // defaultShippingAddress:
 // zipCode:
 // role:
