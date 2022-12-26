7eb56c9bdd95909f6d9b400286bc4a2f2f9004c6
Alberto Martinez <srbetito85@gmail.com>
2022-12-26 20:12:33 -0300

    qwsse

--- api/middlewares/credentials.js
+++ api/middlewares/credentials.js
@@ -2,6 +2,7 @@ const allowedOrigins = require("../config/allowOrigins");
 
 function credentials(req, res, next) {
   const origin = req.headers.origin;
+  console.log("ORIGIN = ", origin);
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
   }
