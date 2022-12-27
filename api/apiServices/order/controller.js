02dfbacd9102dd7ea3b2668864a4e0db28f8e0bd
Matyarg <matias.g15@hotmail.com>
2022-12-27 17:55:30 -0300

    get orders funcionando y con datos limpios

--- api/apiServices/order/controller.js
+++ api/apiServices/order/controller.js
@@ -1,4 +1,6 @@
-const { Order } = require("../../services/db/db.js");
+const { Order,Product, OrderProduct } = require("../../services/db/db.js");
+const Sequelize = require("sequelize");
+
 
 async function createOrder({ status, user_id, products }) {
   //product = [product_id,product_id,product_id,product_id]
@@ -21,23 +23,72 @@ async function createOrder({ status, user_id, products }) {
 
 async function getOrders() {
   try {
-    const orderById = await Order.findAll();
-    return orderById;
+    const getOrders = await Order.findAll();
+    return getOrders;
   } catch (error) {
     throw new Error(error.message);
   }
 }
 
-async function getOrderById(order_id) {
+async function getOrderById(order_id) { // busca por order o por user id
   try {
-    const orderById = await Order.findAll({
-      where: { order_id },
-    });
-    return orderById;
+    const orde1 = await Order.findAll({
+			where: { order_id },
+			include: {
+        model: Product,
+				attributes:['product_id'],
+				through: {
+          attributes: ["count"],
+				},
+			},
+		});
+
+    const orderById = orde1.map((el) => { //ordenamos los datos para mandarlos limpios al front
+      return  {
+        order_id: el.order_id,
+        status: el.status,
+        user_id: el.user_id,
+        products: el.Products.map((el) => {return { product_id: el.product_id,
+        count: el.OrderProduct.count}}),
+      }
+    })
+
+		return orderById;
   } catch (error) {
     throw new Error(error.message);
   }
 }
+
+async function getOrderByUserId(user_id) {	// busca por order o por user id
+	try {
+		const orde1 = await Order.findAll({
+			where: { user_id },
+			include: {
+        model: Product,
+				attributes:['product_id'],
+				through: {
+          attributes: ["count"],
+				},
+			},
+		});
+
+    const clearResponse = orde1.map((el) => { //ordenamos los datos para mandarlos limpios al front
+      return  {
+        order_id: el.order_id,
+        status: el.status,
+        user_id: el.user_id,
+        products: el.Products.map((el) => {return { product_id: el.product_id,
+        count: el.OrderProduct.count}}),
+      }
+    })
+
+		return clearResponse;
+	} catch (error) {
+		throw new Error(error.message);
+	}
+}
+
+
 
 async function updateOrder(order,data) {
   try {
@@ -47,4 +98,4 @@ async function updateOrder(order,data) {
     throw new Error(error);
   }
 }
-module.exports = { createOrder,getOrderById,updateOrder,getOrders };
+module.exports = { createOrder,getOrderById,updateOrder,getOrders , getOrderByUserId};
