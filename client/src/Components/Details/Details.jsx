8fb001743250edb637620745b3c992c342f8db94
Santiago <larriquesantiago@gmail.com>
2022-12-27 12:16:47 -0300

    asdddfasdsa

--- client/src/Components/Details/Details.jsx
+++ client/src/Components/Details/Details.jsx
@@ -1,97 +1,107 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom';
-import * as actions from '../../redux/actions'
-import Footer from '../Footer/Footer';
-import NavBar from '../NavBar/NavBar';
-import s from './Details.module.css';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart, faStar, faTruck, faStore} from "@fortawesome/free-solid-svg-icons";
-import Dropdown from '../Dropdown/Dropdown';
-import Carrusel from '../Carrusel/Carrusel';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import * as actions from "../../redux/actions";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+import s from "./Details.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faStar,
+  faTruck,
+  faStore,
+} from "@fortawesome/free-solid-svg-icons";
+import Dropdown from "../Dropdown/Dropdown";
+import Carrusel from "../Carrusel/Carrusel";
 
 function Details() {
-  const { id } = useParams()
-  const product = useSelector(state => state.detail);
+  const { id } = useParams();
+  const product = useSelector((state) => state.detail);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(actions.getProductById(id))
-  },[dispatch,id])
+  useEffect(() => {
+    dispatch(actions.getProductById(id));
+    console.log(product);
+    console.log(stock);
+  }, [dispatch, id]);
 
+  const [quantity, setQuantity] = useState(0);
+  const [stock, setStock] = useState(product.stock);
 
-  const [quantity, setQuantity] = useState(0)
-  const [stock, setStock]= useState(parseInt(product.stock))
-  const handlePlus = () =>{
-    if(quantity < product.stock){
-      setQuantity(quantity+1);
-      setStock(stock-1);
+  const handlePlus = () => {
+    if (quantity < product.stock) {
+      setQuantity(quantity + 1);
+      setStock(stock - 1);
     }
-  }
+  };
 
-  const handleMinus = ()=>{
-    if(quantity> 0){
-      setQuantity(quantity-1);
-      setStock(stock+1)
+  const handleMinus = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      setStock(stock + 1);
     }
-  }
-  
-  const description = product.description?.map(ele => {
+  };
+
+  const description = product.description?.map((ele) => {
     const key = Object.keys(ele)[0];
     const value = Object.values(ele)[0];
 
     switch (key) {
-      case 'ul':
-        return (<ul>{value.map(data => {
-          return <li>{data}</li>
-        })}</ul>);
-      case 'p':
-        return <p>{value}</p>            
+      case "ul":
+        return (
+          <ul>
+            {value.map((data) => {
+              return <li>{data}</li>;
+            })}
+          </ul>
+        );
+      case "p":
+        return <p>{value}</p>;
       default:
         return <br />;
     }
-  })
+  });
 
-// Inicio de Lógica Comentarios
-  
+  // Inicio de Lógica Comentarios
 
   const [score, setScore] = useState({
-    rating: '',
-    review:'',
-  })
-  const [comments, setComments] = useState([])
+    rating: "",
+    review: "",
+  });
+  const [comments, setComments] = useState([]);
 
   const handleChange = (e) => {
     setScore({
       ...score,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setComments([...comments, score])
+    setComments([...comments, score]);
     setScore({
-      rating:'',
-      review:'',
-    })
-  }
+      rating: "",
+      review: "",
+    });
+  };
 
   const averageReview = (c) => {
-    const numbers = []
+    const numbers = [];
     for (let i = 0; i < c.length; i++) {
-      if(c[i].rating === '★☆☆☆☆') numbers.push(1);        
-      if(c[i].rating === '★★☆☆☆') numbers.push(2);        
-      if(c[i].rating === '★★★☆☆') numbers.push(3);        
-      if(c[i].rating === '★★★★☆') numbers.push(4);        
-      if(c[i].rating === '★★★★★') numbers.push(5);        
+      if (c[i].rating === "★☆☆☆☆") numbers.push(1);
+      if (c[i].rating === "★★☆☆☆") numbers.push(2);
+      if (c[i].rating === "★★★☆☆") numbers.push(3);
+      if (c[i].rating === "★★★★☆") numbers.push(4);
+      if (c[i].rating === "★★★★★") numbers.push(5);
     }
     const sum = numbers.reduce((prev, curr) => prev + curr);
-    const average = Math.ceil(sum/c.length);
-    return average
-  }
+    const average = Math.ceil(sum / c.length);
+    return average;
+  };
 
-// Fin de Lógica Comentarios
+  // Fin de Lógica Comentarios
 
   return (
     <div className={s.detailPage}>
@@ -112,36 +122,63 @@ function Details() {
             </div>
           </div>
         </div>
-        <div className={s.productCart} >
-          <span className={s.pId} >ID Producto: {product.product_id} </span>
+        <div className={s.productCart}>
+          <span className={s.pId}>ID Producto: {product.product_id} </span>
           <h2 className={s.pBrand}>{product.brand}</h2>
           <h1 className={s.pName}>{product.name}</h1>
 
           <div className={s.pScore}>
-            {comments.length ? new Array(averageReview(comments)).fill(undefined).map((ele, idx)=>(
-              <FontAwesomeIcon icon={faStar} key={idx} />
-            )) : <span>Sin puntuación</span> }
+            {comments.length ? (
+              new Array(averageReview(comments))
+                .fill(undefined)
+                .map((ele, idx) => <FontAwesomeIcon icon={faStar} key={idx} />)
+            ) : (
+              <span>Sin puntuación</span>
+            )}
           </div>
           <div className={s.delivery}>
             <h3>Tipo de Entrega</h3>
             <div>
-              <input type="radio" id="contactChoice1" name="contact" value="email" />
-              <label>&nbsp;&nbsp;<FontAwesomeIcon icon={faTruck} /> &nbsp;&nbsp;Despacho a domicilio </label>
+              <input
+                type="radio"
+                id="contactChoice1"
+                name="contact"
+                value="email"
+              />
+              <label>
+                &nbsp;&nbsp;
+                <FontAwesomeIcon icon={faTruck} /> &nbsp;&nbsp;Despacho a
+                domicilio{" "}
+              </label>
             </div>
             <div>
-              <input type="radio" id="contactChoice1" name="contact" value="email" />
-              <label>&nbsp;&nbsp;<FontAwesomeIcon icon={faStore} /> &nbsp;&nbsp;Retiro en tienda </label>
+              <input
+                type="radio"
+                id="contactChoice1"
+                name="contact"
+                value="email"
+              />
+              <label>
+                &nbsp;&nbsp;
+                <FontAwesomeIcon icon={faStore} /> &nbsp;&nbsp;Retiro en tienda{" "}
+              </label>
             </div>
           </div>
           <hr />
           <h2 className={s.price}>US${product.price}</h2>
-          <div className={s.quantity} >
+          <div className={s.quantity}>
             <div>
-              <button onClick={handleMinus} >-</button>&nbsp;&nbsp;&nbsp;&nbsp;{quantity}&nbsp;&nbsp;&nbsp;&nbsp;<button onClick={handlePlus}>+</button>
+              <button onClick={handleMinus}>-</button>&nbsp;&nbsp;&nbsp;&nbsp;
+              {quantity}&nbsp;&nbsp;&nbsp;&nbsp;
+              <button onClick={handlePlus}>+</button>
             </div>
-            <span className={s.stock} >&nbsp;&nbsp;&nbsp;&nbsp;Stock disponible: {parseInt(stock)} </span>
+            <span className={s.stock}>
+              &nbsp;&nbsp;&nbsp;&nbsp;Stock disponible: {parseInt(stock)}{" "}
+            </span>
           </div>
-          <button type='submit' className={s.mainButton}>Agregar al Carrito</button>
+          <button type="submit" className={s.mainButton}>
+            Agregar al Carrito
+          </button>
         </div>
       </section>
 
@@ -157,7 +194,7 @@ function Details() {
         <br />
         <Carrusel />
       </section>
-      
+
       <br />
       <div className={s.sub}>
         <div className={s.subTitles}>
@@ -165,28 +202,46 @@ function Details() {
           <span></span>
         </div>
       </div>
-      
+
       <section className={s.commentSection}>
         <div className={s.allComments}>
-          <h5><strong>Cantidad de comentarios:</strong> &nbsp;&nbsp; {comments.length}</h5>
-          <hr />        
+          <h5>
+            <strong>Cantidad de comentarios:</strong> &nbsp;&nbsp;{" "}
+            {comments.length}
+          </h5>
+          <hr />
           <div>
             {/* Sección que muestra comentarios existentes */}
-            {comments.length? comments.map(c => (
-            <div className={s.newComment}>
-              <span className={s.userId}><strong>Por:</strong></span>
-              <span className={s.rating} >{c.rating} </span>
-              <p className={s.review}>{c.review}</p>
-              <hr />
-            </div>)) : <p className={s.noReview}>Este producto aún no tiene reseñas. ¡Sé el primero en compartirnos tu opinión!</p>  }
-            
+            {comments.length ? (
+              comments.map((c) => (
+                <div className={s.newComment}>
+                  <span className={s.userId}>
+                    <strong>Por:</strong>
+                  </span>
+                  <span className={s.rating}>{c.rating} </span>
+                  <p className={s.review}>{c.review}</p>
+                  <hr />
+                </div>
+              ))
+            ) : (
+              <p className={s.noReview}>
+                Este producto aún no tiene reseñas. ¡Sé el primero en
+                compartirnos tu opinión!
+              </p>
+            )}
+
             {/* Sección de para crear comentarios */}
-            
 
             <form className={s.cForm} onSubmit={handleSubmit}>
               <div>
-                <label className={s.label}>Valoración de tu compra&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                <select name="rating" value={score.rating} onChange={handleChange} >
+                <label className={s.label}>
+                  Valoración de tu compra&nbsp;&nbsp;&nbsp;&nbsp;
+                </label>
+                <select
+                  name="rating"
+                  value={score.rating}
+                  onChange={handleChange}
+                >
                   <option>seleccionar</option>
                   <option value="★☆☆☆☆">★☆☆☆☆</option>
                   <option value="★★☆☆☆">★★☆☆☆</option>
@@ -195,9 +250,19 @@ function Details() {
                   <option value="★★★★★">★★★★★</option>
                 </select>
               </div>
-              <textarea rows="5" name="review" value={score.review} placeholder='Escribe un comentario sobre tu compra' onChange={handleChange} />
+              <textarea
+                rows="5"
+                name="review"
+                value={score.review}
+                placeholder="Escribe un comentario sobre tu compra"
+                onChange={handleChange}
+              />
               <br />
-              <input className={s.mainButton} type="submit" value="Agregar Comentario" />
+              <input
+                className={s.mainButton}
+                type="submit"
+                value="Agregar Comentario"
+              />
             </form>
           </div>
         </div>
@@ -205,10 +270,10 @@ function Details() {
       <br />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Details
+export default Details;
 
 //Extra al formulario de comentario
 // Faltan las validaciones del formulario. Es necesario que cuando se clickee el boton, se haya recibido todo, es decir, user_id, product_id, review y rating.
