42555aaf388ba38d180773903c700b6647682e1f
Alissa Gamarra <alissa.gamarra@gmail.com>
2022-12-27 12:32:58 -0500

    CardH, CardCart, Página de Favoritos y Página de Carrito

--- client/src/Components/Favoritos/Favoritos.jsx
+++ client/src/Components/Favoritos/Favoritos.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { useSelector } from "react-redux";
-import CardV from '../Card V/CardV';
+import CardH from '../Card H/CardH';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
+import s from './Favoritos.module.css'
 
 function Favoritos() {
     const favs = useSelector(state => state.favorites)
     return (
         <div>
             <NavBar />
-            {favs.length ? favs.map(p => <CardV 
-                key={p.id}
-                id={p.id}
-                brand={p.brand}
-                name={p.name}
-                image={p.image}
-                price={p.price}
-            />)
-            : <p>'Todavía no has agregado productos favoritos'</p>}
+            <section className={s.favSection}>
+                {favs.length ? favs.map(p => <CardH 
+                key={p.id} id={p.id}
+                stock={p.stock} brand={p.brand}
+                name={p.name} image={p.image} price={p.price}/>)
+            : <p>Todavía no has agregado productos favoritos</p>}
+            </section>
             <Footer/>
         </div>
     )
