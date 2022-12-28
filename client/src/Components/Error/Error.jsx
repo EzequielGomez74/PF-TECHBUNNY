2ab8f52ccad56b6383b501fb6ef98ef363575c8c
Alissa Gamarra <alissa.gamarra@gmail.com>
2022-12-28 10:41:07 -0500

    Página de Error con estilos y funcionalidad

--- client/src/Components/Error/Error.jsx
+++ client/src/Components/Error/Error.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
+import s from './Error.module.css'
+import { useHistory } from 'react-router-dom';
 
 function Error() {
+    const history = useHistory();
+    const handleClick = () => {
+        history.push('/home');
+    }
+
   return (
     <div>
         <NavBar />
-        <div>
-            <p>404</p>
-            <div></div>
-            <p>Esta página no existe</p>
-            <button>Regresar al Home</button>
+        <div className={s.errorPage}>
+            <p className={s.message}>¡Oops! Esta página no existe</p>
+            <div className={s.heroError}></div>
+            <button className={s.mainButton} onClick={handleClick}>Regresar al Home</button>
         </div>
         <Footer />
     </div>
