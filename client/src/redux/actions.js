7ca1bfce731a03c444d591dc579ec1e247ddd6de
EzequielGomez74 <ezequiel.canob@gmail.com>
2022-12-27 23:42:12 -0300

    asd

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,7 +2,7 @@ import { GET_ALL_PRODUCTS, GET_CATEGORIES, GET_PRODUCT_BY_ID, GET_PRODUCTS_BY_CA
 
 export const getProducts = () => {
     return function(dispatch){
-        return fetch ('http://localhost:3001/products')
+        return fetch ('/products')
         .then(resp => resp.json())
         .then(data => dispatch({type: GET_ALL_PRODUCTS, payload: data}))
         .catch(error => console.log(error))
@@ -10,7 +10,7 @@ export const getProducts = () => {
 }
 export const getProductById = (id) => {
     return function(dispatch){
-        return fetch (`http://localhost:3001/products/${id}`)
+        return fetch (`/products/${id}`)
         .then(resp => resp.json())
         .then(data => dispatch({type: GET_PRODUCT_BY_ID, payload: data}))
         .catch(error => console.log(error))
@@ -19,7 +19,7 @@ export const getProductById = (id) => {
 
 export const getCategories = () => {
     return function(dispatch){
-        return fetch ('http://localhost:3001/categories')
+        return fetch ('/categories')
         .then(resp => resp.json())
         .then(data => dispatch({type: GET_CATEGORIES, payload: data}))
         .catch(error => console.log(error))
@@ -29,7 +29,7 @@ export const getCategories = () => {
 export const getProductsByCategory = (category) => {
     return function(dispatch){
         // let Capitalize = category[0].toUpperCase() + category.slice(1)
-        return fetch (`http://localhost:3001/products?category=${category}`)
+        return fetch (`/products?category=${category}`)
         .then(resp => resp.json())
         .then(data => dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: data}))
         .catch(error => console.log(error))
