14a2070d3f595ff7af382dd13012c6e0cbe9b43f
abibreno <jenniferabigailbreno@gmail.com>
2022-12-28 12:44:26 -0300

    actions modificadas a axios, falta traer lo de beto y cambiar detalle de ruta

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,40 +1,89 @@
+// import { bindActionCreators } from 'redux'
+import axios from "axios";
 import { GET_ALL_PRODUCTS, GET_CATEGORIES, GET_PRODUCT_BY_ID, GET_PRODUCTS_BY_CATEGORY, FILTER_BY_BRAND, FILTER_BY_PRICE, ORDER_BY_PRICE, ADD_FAVORITE, ADD_CART, REMOVE_CART, REMOVE_FAVORITE } from './actionTypes'
 
-export const getProducts = () => {
-    return function(dispatch){
-        return fetch ('http://localhost:3001/products')
-        .then(resp => resp.json())
-        .then(data => dispatch({type: GET_ALL_PRODUCTS, payload: data}))
-        .catch(error => console.log(error))
-    }
-}
-export const getProductById = (id) => {
-    return function(dispatch){
-        return fetch (`http://localhost:3001/products/${id}`)
-        .then(resp => resp.json())
-        .then(data => dispatch({type: GET_PRODUCT_BY_ID, payload: data}))
-        .catch(error => console.log(error))
+
+export function getProducts () {
+    return async function (dispatch){
+        try{
+            var json = await axios.get('http://localhost:3001/products')
+            return dispatch({type: GET_ALL_PRODUCTS, payload: json.data});
+        }catch(error){
+            alert(error)
+        }
     }
 }
 
-export const getCategories = () => {
-    return function(dispatch){
-        return fetch ('http://localhost:3001/categories')
-        .then(resp => resp.json())
-        .then(data => dispatch({type: GET_CATEGORIES, payload: data}))
-        .catch(error => console.log(error))
+// export const getProducts = () => {
+//     return function(dispatch){
+//         return fetch ('http://localhost:3001/products')
+//         .then(resp => resp.json())
+//         .then(data => dispatch({type: GET_ALL_PRODUCTS, payload: data}))
+//         .catch(error => console.log(error))
+//     }
+// }
+
+export function getProductById(id) {
+    return async function (dispatch){
+        try{
+            var json = await axios.get(`http://localhost:3001/products/${id}`)
+            return dispatch({type: GET_PRODUCT_BY_ID, payload: json.data});
+        }catch(error){
+            alert(error)
+        }
     }
 }
 
-export const getProductsByCategory = (category) => {
-    return function(dispatch){
-        // let Capitalize = category[0].toUpperCase() + category.slice(1)
-        return fetch (`http://localhost:3001/products?category=${category}`)
-        .then(resp => resp.json())
-        .then(data => dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: data}))
-        .catch(error => console.log(error))
+// export const getProductById = (id) => {
+//     return function(dispatch){
+//         return fetch (`http://localhost:3001/products/${id}`)
+//         .then(resp => resp.json())
+//         .then(data => dispatch({type: GET_PRODUCT_BY_ID, payload: data}))
+//         .catch(error => console.log(error))
+//     }
+// }
+
+export function getCategories () {
+    return async function (dispatch){
+        try{
+            var json = await axios.get('http://localhost:3001/categories')
+            return dispatch({type: GET_CATEGORIES, payload: json.data});
+        }catch(error){
+            alert(error)
+        }
     }
 }
+
+// export const getCategories = () => {
+//     return function(dispatch){
+//         return fetch ('http://localhost:3001/categories')
+//         .then(resp => resp.json())
+//         .then(data => dispatch({type: GET_CATEGORIES, payload: data}))
+//         .catch(error => console.log(error))
+//     }
+// }
+
+export function getProductsByCategory(category) {
+    return async function (dispatch){
+        try{
+            var json = await axios.get(`http://localhost:3001/products?category=${category}`)
+            return dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: json.data});
+        }catch(error){
+            alert(error)
+        }
+    }
+}
+
+// export const getProductsByCategory = (category) => {
+//     return function(dispatch){
+//         // let Capitalize = category[0].toUpperCase() + category.slice(1)
+//         return fetch (`http://localhost:3001/products?category=${category}`)
+//         .then(resp => resp.json())
+//         .then(data => dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: data}))
+//         .catch(error => console.log(error))
+//     }
+// }
+
 
 export const filterByBrand = (products, brand) => {
     return function(dispatch){
