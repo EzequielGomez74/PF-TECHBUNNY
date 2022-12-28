49feb38b71f435425c2530c05769bbfeb41dbb5f
gerlini <gerfer97.gf@gmail.com>
2022-12-28 10:28:19 -0300

    emailer

---
+++ api/apiServices/email/controller.js
@@ -0,0 +1,9 @@
+import { transporter } from "../../config/emailer.js";
+
+let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
