1f0287b720e264ef6144c0a9eecffed70301a9fa
Gerlini <gerfer97.gf@gmail.com>
2022-12-29 01:11:44 -0300

    Merge con matyy

--- api/apiServices/enter/controller.js
+++ api/apiServices/enter/controller.js
@@ -36,27 +36,29 @@ async function handleNewUser(data) {
 }
 async function handleLogin(username, password) {
   if (!username || !password)
-    throw new Error("Username and Password are required");
+  throw new Error("Username and Password are required");
   try {
     const foundUser = await User.findOne({ where: { username: username } });
     if (!foundUser) throw new Error("Unauthorized user"); //401 = unauthorized
     //evaluar password
+    console.log("LLEGA");
     //const match = await bcrypt.compare(password, password);
     if (true) {
-      //!! ACA HAY QUE CREAR EL JWT VALIDATOR TOKEN !! json web token (access token - refresh token)
+      //!! ACA HAY QUE CREAR EL JWT VALIDATOR TOKEN !! json web token (access token - refresh token)      
       const accessToken = jwt.sign(
         { username: foundUser.username, role: foundUser.role },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "60s" }
+        { expiresIn: "600s" }
       );
       const refreshToken = jwt.sign(
         { username: foundUser.username },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: "120s" }
+        { expiresIn: "1200s" }
       );
       //guardar el refreshToken en la DB
       foundUser.set({ refreshToken: refreshToken });
       await foundUser.save();
+      
       return { accessToken, refreshToken };
     } else throw new Error("Wrong Password");
   } catch (error) {
