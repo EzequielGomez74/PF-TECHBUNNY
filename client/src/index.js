66da7ed17137eb33238985b0d8dab959ce7b62be
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-01-02 23:19:05 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001/";
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
