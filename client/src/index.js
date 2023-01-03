e86a85681d488ac5270ef3051f357b2ae7409e2f
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-01-03 10:44:55 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import axios from "axios";
+
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001/";
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +17,7 @@ root.render(
       <App />
     </BrowserRouter>
   </Provider>
-);
+); // tuki
 
 // ReactDOM.render(
 //   <Provider store={store}>
