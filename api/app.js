3bc1d5dd0cf819e772b46e91d8523b5776262a02
Alberto Martinez <srbetito85@gmail.com>
2023-01-03 11:28:18 -0300

    a

--- api/app.js
+++ api/app.js
@@ -13,15 +13,21 @@ require("./services/db/db.js");
 const server = express();
 server.name = "API";
 //asd
+console.log("0");
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+console.log("1");
 server.use(accessControl);
+console.log("2");
 server.use(credentials);
+console.log("3");
 server.use(cors(corsOptions));
+console.log("4");
 
 server.use("/", indexRouter);
+
 // server.use(cuatrocientoscuatro)
 // Error catching endware.
 server.use(generalErrorHandler);
