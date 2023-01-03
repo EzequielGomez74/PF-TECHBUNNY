6ad0c2047fbf6e49c68045c66eb09d0bd3d4ea3b
Alberto Martinez <srbetito85@gmail.com>
2023-01-03 12:21:37 -0300

    a

--- client/src/redux/axiosInstance.js
+++ client/src/redux/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  //baseURL: "http://localhost:3001",
+  baseURL: "https://prueba1-production-4ff1.up.railway.app/",
   headers: {
     "Content-Type": "application/json",
   },
