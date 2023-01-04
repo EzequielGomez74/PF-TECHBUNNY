4c878e3e68e3ba3a9556d49d89d91eb3532cfee3
abibreno <jenniferabigailbreno@gmail.com>
2023-01-04 17:24:53 -0300

    cambios front 4/1

--- client/src/Components/Landing/LandingPage.jsx
+++ client/src/Components/Landing/LandingPage.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./LandingPage.css";
+import "./LandingPage.scss";
+import img from '../../Photos/banner1.png'
 
 export default function LandingPage() {
   return (
     <div className="landing">
       <div className="divTitulo">
       <Link to="/home">
-       <h1>TECHBUNNY</h1>
+       <img src={img} className="banner" />
       </Link>
       </div>
-      <div className="divBtn">
-       <div className="btnLogin">
-        <Link to="/">
-         <h2>Login</h2>
-        </Link>
-       </div>
-       <div className="btnInv">
-        <Link to="/home">
-         <h2>Invitado</h2>
-        </Link>
-       </div>
-      </div>
-    </div>
+     
+       <p class="typing-animation">¡Bienvenido a nuestro ecommerce de tecnología!  <br/>Somos una empresa líder en la venta de productos de alta calidad  <br/>y con las últimas innovaciones en el mercado. 
+        <br/>
+          
+
+       Nuestro objetivo es ofrecerte lo mejor de la tecnología a precios competitivos  <br/>y con un servicio de atención al cliente excepcional. 
+       
+        <br/>
+        No dudes en echar un vistazo a nuestro catálogo y  <br/>descubrir todo lo que tenemos para ofrecerte. ¡Te esperamos!
+        </p>
+      
+        
+     </div>
   );
 }
