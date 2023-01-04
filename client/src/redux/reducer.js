96cf8c31a2907b48542095a5d7dbadc254e79d7f
Alberto Martinez <srbetito85@gmail.com>
2023-01-04 01:37:43 -0300

    deploy ready :)

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,89 +1,95 @@
 const initialState = {
-  products: [],
-  detail: {},
-  categories: [],
-  productsByCategory: [],
-  filtered: [],
-  cart: [],
-  favorites: [],
-  darkMode: false,
-  reviews: [],
-  currentUser: {},
-};
+    products:[],
+    detail: {},
+    categories: [],
+    productsByCategory:[],
+    filtered: [],
+    cart: [],
+    favorites: [],
+    darkMode: false,
+    // searchTerm:'',
+    // searchResults:[],
+    results:[]
+}
 
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "GET_ALL_PRODUCTS":
-      return {
-        ...state,
-        products: action.payload,
-      };
-    case "GET_PRODUCT_BY_ID":
-      return {
-        ...state,
-        detail: action.payload,
-      };
-      case "GET_PAYPREFERENCES_BY_ID":
-        return {
-          ...state,
-          detail: action.payload,
-        };
-    case "GET_PRODUCTS_BY_CATEGORY":
-      return {
-        ...state,
-        productsByCategory: action.payload,
-      };
-    case "GET_REVIEWS_BY":
-      return {
-        ...state,
-        reviews: action.payload,
-      };
-    case "GET_CATEGORIES":
-      return {
-        ...state,
-        categories: action.payload,
-      };
-    case "FILTER_BY_BRAND":
-      return {
-        ...state,
-        filtered: action.payload,
-      };
-    case "FILTER_BY_PRICE":
-      return {
-        ...state,
-        filtered: action.payload,
-      };
-    case "ORDER_BY_PRICE":
-      return {
-        ...state,
-        filtered: action.payload,
-      };
-    case "ADD_FAVORITE":
-      return {
-        ...state,
-        favorites: [...state.favorites, action.payload],
-      };
-    case "REMOVE_FAVORITE":
-      return {
-        ...state,
-        favorites: state.favorites.filter((f) => f.id !== action.payload),
-      };
-    case "ADD_CART":
-      return {
-        ...state,
-        cart: [...state.cart, action.payload],
-      };
-    case "REMOVE_CART":
-      return {
-        ...state,
-        cart: state.cart.filter((c) => c.id !== action.payload),
-      };
-    case "TOGGLE_DARK_MODE":
-      return {
-        ...state,
-        darkMode: !state.darkMode,
-      };
-    default:
-      return { ...state };
-  }
+export default function reducer (state=initialState, action){
+    switch (action.type) {
+        case 'GET_ALL_PRODUCTS':
+            return {
+                ...state,
+                products: action.payload,
+            }
+        case 'GET_PRODUCT_BY_ID':
+            return {
+                ...state,
+                detail: action.payload,
+            }
+        case 'GET_PRODUCTS_BY_CATEGORY':
+            return{
+                ...state,
+                productsByCategory: action.payload,
+            }
+        case 'GET_CATEGORIES':
+            return{
+                ...state,
+                categories: action.payload,
+            }
+        case 'FILTER_BY_BRAND':
+            return{
+                ...state,
+                filtered: action.payload,
+            }
+        case 'FILTER_BY_PRICE':
+            return{
+                ...state,
+                filtered: action.payload,
+            }
+        case 'ORDER_BY_PRICE':
+            return{
+                ...state,
+                filtered: action.payload,
+            }
+        case 'ADD_FAVORITE':
+            return{
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }
+        case 'REMOVE_FAVORITE':
+            return{
+                ...state,
+                favorites: state.favorites.filter(f => f.id !== action.payload)
+            }
+        case 'ADD_CART':
+            return{
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        case 'REMOVE_CART':
+            return{
+                ...state,
+                cart: state.cart.filter(c => c.id !== action.payload)
+            }
+        case 'TOGGLE_DARK_MODE':
+                return {
+                  ...state,
+                  darkMode: !state.darkMode
+                };
+        // case 'SET_SEARCH_TERM':
+        //     return {
+        //         ...state,
+        //         searchTerm: action.searchTerm
+        //     }
+        // case 'SET_SEARCH_RESULTS':
+        //     return {
+        //         ...state,
+        //         searchResults: action.results,
+        //     }
+        case 'GET_SEARCH_RESULTS':
+            return {
+                ...state,
+                results: action.payload,
+            }
+        default:
+            return {...state}
+    }
 }
