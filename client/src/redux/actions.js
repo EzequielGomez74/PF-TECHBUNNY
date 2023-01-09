cea5704005437b5da93b4d72528cd6d363c9bc56
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-01-09 14:38:08 -0500

    Modificación Filtros

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,97 +1,126 @@
-import { GET_ALL_PRODUCTS, GET_CATEGORIES, GET_PRODUCT_BY_ID, GET_PRODUCTS_BY_CATEGORY, FILTER_BY_BRAND, ORDER_BY_PRICE, ADD_FAVORITE, ADD_CART, REMOVE_CART, REMOVE_FAVORITE, TOGGLE_DARK_MODE, GET_SEARCH_RESULTS, COMPLETE_PROFILE, GET_REVIEWS_BY, STATUS_REGISTER, GET_SEARCH_TERM, GET_RESULTS } from './actionTypes'
-// import { bindActionCreators } from 'redux'
-// import axiosInstance from "./axiosInstance";
-import axios from "axios";
-// import jwt from 'jsonwebtoken'
+// import store from "./store";
+import axiosInstance from "./axiosInstance";
+// import axios from "axios";
+import {
+  GET_ALL_PRODUCTS,
+  GET_CATEGORIES,
+  GET_PRODUCT_BY_ID,
+  GET_PRODUCTS_BY_CATEGORY,
+  FILTER_BY_BRAND,
+  ADD_FAVORITE,
+  ADD_CART,
+  REMOVE_CART,
+  REMOVE_FAVORITE,
+  TOGGLE_DARK_MODE,
+  GET_SEARCH_RESULTS,
+  GET_REVIEWS_BY,
+  SET_LOGGED_USER,
+  SORT_BY_PRICE
+} from "./actionTypes";
 
-
-export function getProducts () {
-    return async function (dispatch){
-        try{
-            var json = await axios.get('http://localhost:3001/products')
-            return dispatch({type: GET_ALL_PRODUCTS, payload: json.data});
-        }catch(error){
-            alert(error)
-        }
+export const getProducts = (id) => {
+  return async function (dispatch) {
+    try {
+      const response = await axiosInstance.get("/products");
+      console.log("Mostrando productos");
+      return dispatch({ type: GET_ALL_PRODUCTS, payload: response.data });
+    } catch (error) {
+      console.log("FAILED TO AUTHENTICATE");
     }
-}
+  };
+};
 
-// export const getProducts = () => {
-//     return function(dispatch){
-//         return fetch ('http://localhost:3001/products')
-//         .then(resp => resp.json())
-//         .then(data => dispatch({type: GET_ALL_PRODUCTS, payload: data}))
-//         .catch(error => console.log(error))
+// export function getProducts() {
+//   return async function (dispatch) {
+//     try {
+//       var json = await axios.get("http://localhost:3001/products");
+//       return dispatch({ type: GET_ALL_PRODUCTS, payload: json.data });
+//     } catch (error) {
+//       alert(error);
 //     }
+//   };
 // }
 
 export function getProductById(id) {
-    return async function (dispatch){
-        try{
-            var json = await axios.get(`http://localhost:3001/products/${id}`)
-            return dispatch({type: GET_PRODUCT_BY_ID, payload: json.data});
-        }catch(error){
-            alert(error)
-        }
+  return async function (dispatch) {
+    try {
+      console.log("#");
+      var json = await axiosInstance.get(`/products/${id}`);
+      return dispatch({ type: GET_PRODUCT_BY_ID, payload: json.data });
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 }
-
-// export const getProductById = (id) => {
-//     return function(dispatch){
-//         return fetch (`http://localhost:3001/products/${id}`)
-//         .then(resp => resp.json())
-//         .then(data => dispatch({type: GET_PRODUCT_BY_ID, payload: data}))
-//         .catch(error => console.log(error))
-//     }
-// }
-
-export function getCategories () {
-    return async function (dispatch){
-        try{
-            var json = await axios.get('http://localhost:3001/categories')
-            return dispatch({type: GET_CATEGORIES, payload: json.data});
-        }catch(error){
-            alert(error)
-        }
+export const getReviewsBy = (productId, userId) => {
+  return async function (dispatch) {
+    try {
+      const response = await axiosInstance.get(
+        `/reviews?product_id=${productId}`
+      );
+      return dispatch({ type: GET_REVIEWS_BY, payload: response.data });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
-// export const getCategories = () => {
-//     return function(dispatch){
-//         return fetch ('http://localhost:3001/categories')
-//         .then(resp => resp.json())
-//         .then(data => dispatch({type: GET_CATEGORIES, payload: data}))
-//         .catch(error => console.log(error))
-//     }
-// }
+export const postReview = (review, onSuccess) => {
+  return async function () {
+    try {
+      console.log("review", review);
+      const postedReview = await axiosInstance.post("/reviews", review);
+      onSuccess();
+      return postedReview;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export function getCategories() {
+  return async function (dispatch) {
+    try {
+      var json = await axiosInstance.get("/categories");
+      return dispatch({ type: GET_CATEGORIES, payload: json.data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+}
 
 export function getProductsByCategory(category) {
-    return async function (dispatch){
-        try{
-            var json = await axios.get(`http://localhost:3001/products?category=${category}`)
-            return dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: json.data});
-        }catch(error){
-            alert(error)
-        }
+  return async function (dispatch) {
+    try {
+      console.log(category);
+      let json = await axiosInstance.get(`/products?category=${category}`);
+      console.log("1");
+      return dispatch({ type: GET_PRODUCTS_BY_CATEGORY, payload: json.data });
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 }
 
-// export const getProductsByCategory = (category) => {
-//     return function(dispatch){
-//         // let Capitalize = category[0].toUpperCase() + category.slice(1)
-//         return fetch (`http://localhost:3001/products?category=${category}`)
-//         .then(resp => resp.json())
-//         .then(data => dispatch({type: GET_PRODUCTS_BY_CATEGORY, payload: data}))
-//         .catch(error => console.log(error))
-//     }
-// }
+export const filterByBrand = (brand) => {
+  return { type: FILTER_BY_BRAND, payload: brand };
+};
 
+export const sortByPrice = (priceOrder) => {
+  return { type: SORT_BY_PRICE, payload: priceOrder };
+};
 
-export const filterByBrand = (products, brand) => {
-    return function(dispatch){
-        const filteredByBrand = products.filter((p) => p.brand === brand)
-        dispatch({type: FILTER_BY_BRAND, payload: filteredByBrand})
-    }
+// export const filterByPrice = (products, max, min) => {
+//   return function (dispatch) {
+//     const filteredByPrice = products.filter(
+//       (p) => p.price < max && p.price > min
+//     );
+//     dispatch({ type: FILTER_BY_PRICE, payload: filteredByPrice });
+//   };
+// };
+
+export function toggleDarkMode() {
+  return { type: TOGGLE_DARK_MODE };
 }
 
 // export const filterByPrice = (products, max, min) => {
@@ -101,59 +130,63 @@ export const filterByBrand = (products, brand) => {
 //     }
 // }
 
-export const orderByPrice = (products, order) => {
-    return function(dispatch){
-        if(order === 'asc'){
-            const asc = products.sort((a,b)=>{
-                if(a.price < b.price) return -1;
-                if(a.price > b.price) return 1;
-                else return 0;
-            })
-            dispatch({type: ORDER_BY_PRICE, payload:[...asc]})
-        }
-        if(order === 'desc'){
-            const desc = products.sort((a,b)=>{
-                if(a.price > b.price) return -1;
-                if(a.price < b.price) return 1;
-                else return 0;
-            })
-            dispatch({type: ORDER_BY_PRICE, payload:[...desc]})
-        }
-    }
-}
+// export const orderByPrice = (products, order) => {
+//   return function (dispatch) {
+//     if (order === "asc") {
+//       const asc = products.sort((a, b) => {
+//         if (a.price < b.price) return -1;
+//         if (a.price > b.price) return 1;
+//         else return 0;
+//       });
+//       dispatch({ type: ORDER_BY_PRICE, payload: [...asc] });
+//     }
+//     if (order === "desc") {
+//       const desc = products.sort((a, b) => {
+//         if (a.price > b.price) return -1;
+//         if (a.price < b.price) return 1;
+//         else return 0;
+//       });
+//       dispatch({ type: ORDER_BY_PRICE, payload: [...desc] });
+//     }
+//   };
+// };
 
 export const addFavorite = (payload) => {
-    return {
-        type: ADD_FAVORITE, payload
-    }
-}
+  return {
+    type: ADD_FAVORITE,
+    payload,
+  };
+};
 
 export const removeFavorite = (id) => {
-    return {
-        type: REMOVE_FAVORITE, payload: id
-    }
-}
-
+  return {
+    type: REMOVE_FAVORITE,
+    payload: id,
+  };
+};
 
 export const addCart = (payload) => {
-    return {
-        type: ADD_CART, payload
-    }
-}
+  return {
+    type: ADD_CART,
+    payload,
+  };
+};
 
 export const removeCart = (id) => {
-    return {
-        type: REMOVE_CART, payload: id
-    }
-}
+  return {
+    type: REMOVE_CART,
+    payload: id,
+  };
+};
 
-export function toggleDarkMode() {
-    return { type: TOGGLE_DARK_MODE };
-  }
-
-export const getSearchTerm = (searchTerm) => {
-    return { type: GET_SEARCH_TERM, payload: searchTerm }
-}
+export const getSearchResults = (products, searchTerm) => {
+  return function (dispatch) {
+    const results = products.filter((p) =>
+      p.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    dispatch({ type: GET_SEARCH_RESULTS, payload: results });
+  };
+};
 
 // export const setSearchTerm = (searchTerm) => {
 //     return {
@@ -167,74 +200,15 @@ export const getSearchTerm = (searchTerm) => {
 //     }
 // }
 
-export const getSearchResults = (products, searchTerm) => {
-    return function(dispatch){
-        const results = products.filter(p => p.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        dispatch({type: GET_SEARCH_RESULTS, payload: results})
-    }
-}
-
-export const getResults = (products, searchTerm) => {
-    return function(dispatch){
-        const results = products.filter(p => p.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        dispatch({type: GET_RESULTS, payload: results})
-    }
-}
-
-export const completeProfile = (payload) => {
-    return { type: COMPLETE_PROFILE, payload }
-}
-
-export const getReviewsBy = (productId, userId) => {
-    return async function (dispatch) {
-      try {
-        const response = await axios.get(
-          `http://localhost:3001/reviews?product_id=${productId}`
-        );
-        return dispatch({ type: GET_REVIEWS_BY, payload: response.data });
-      } catch (error) {
-        console.log(error);
-      }
-    };
+export const getUser = () => {
+  return async function (dispatch) {
+    const user = await axiosInstance.get("/");
   };
-  
-export const postReview = (review, onSuccess) => {
-    return async function () {
-      try {
-        let postedReview = await axios.post(
-          "http://localhost:3001/reviews",
-          review
-        );
-        onSuccess();
-        return postedReview;
-      } catch (error) {
-        console.log(error);
-      }
-    };
 };
 
-// // Traer el nuevo usuario registrado-PENDIENTE
-// export const getNewRegisteredUser= (user_id) => {
-//     return async function(dispatch){
-//         try{
-//             // Verificar que el user_id sea de un usuario válido
-//             l
-//         }catch(error){
-//             alert(error)
-//         }
-//     }
-// }
-
-
-//Verificación del Mail
-export const statusRegister = (token) => {
-    return async function(dispatch) {
-        try{
-            let validate = await axios.put(`http://localhost:3001/verify/${token}`)
-            // if validate.data.notAccepted === 'FAIL' || 'SUCCESS'
-            return dispatch({type: STATUS_REGISTER, payload: validate.data.status})
-        }catch(error){
-            alert(error)
-        }
-    }
-}
+export const setLoggedUser = (user) => {
+  return {
+    type: SET_LOGGED_USER,
+    payload: user,
+  };
+};
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,126 +1,149 @@
 const initialState = {
-    products:[],
+    products: [],
     detail: {},
     categories: [],
-    productsByCategory:[],
+    productsByCategory: [],
     filtered: [],
     cart: [],
     favorites: [],
     darkMode: false,
+    reviews: [],
     // searchTerm:'',
     // searchResults:[],
-    results:[],
-    results2:[],
-    reviews: [],
-    currentUser: {},
-    // user: false,
-    tokenAccepted: '',
-    searchTerm: '',
-}
-
-export default function reducer (state=initialState, action){
+    results: [],
+    loggedUser: {},
+  };
+  
+  export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case 'GET_ALL_PRODUCTS':
-            return {
-                ...state,
-                products: action.payload,
+      case "GET_ALL_PRODUCTS":
+        return {
+          ...state,
+          products: action.payload,
+        };
+      case "GET_PRODUCT_BY_ID":
+        return {
+          ...state,
+          detail: action.payload,
+        };
+      case "GET_PRODUCTS_BY_CATEGORY":
+        return {
+          ...state,
+          productsByCategory: action.payload,
+          filtered: action.payload
+        };
+      case "GET_CATEGORIES":
+        return {
+          ...state,
+          categories: action.payload,
+        };
+      case "FILTER_BY_BRAND":
+          const allProductsByCategory = [...state.filtered];
+          const filteredProducts =
+            action.payload === "none"
+              ? allProductsByCategory
+              : allProductsByCategory.filter((p) =>
+                  p.brand.includes(action.payload)
+                );
+          return {
+            ...state,
+            productsByCategory: filteredProducts,
+          };
+      // case "FILTER_BY_PRICE":
+      //   return {
+      //     ...state,
+      //     filtered: action.payload,
+      //   };
+      case "SORT_BY_PRICE":
+          const orderedProductsByPrice = state.productsByCategory.sort(function (
+            a,
+            b
+          ) {
+            if (action.payload === "asc") {
+              if (a.price < b.price) {
+                return -1;
+              } else if (a.price > b.price) {
+                return 1;
+              } else {
+                return 0;
+              }
+            } else if (action.payload === "desc") {
+              if (a.price > b.price) {
+                return -1;
+              } else if (a.price < b.price) {
+                return 1;
+              } else {
+                return 0;
+              }
             }
-        case 'GET_PRODUCT_BY_ID':
-            return {
-                ...state,
-                detail: action.payload,
-            }
-        case 'GET_PRODUCTS_BY_CATEGORY':
-            return{
-                ...state,
-                productsByCategory: action.payload,
-            }
-        case 'GET_CATEGORIES':
-            return{
-                ...state,
-                categories: action.payload,
-            }
-        case 'FILTER_BY_BRAND':
-            return{
-                ...state,
-                filtered: action.payload,
-            }
-        case 'FILTER_BY_PRICE':
-            return{
-                ...state,
-                filtered: action.payload,
-            }
-        case 'ORDER_BY_PRICE':
-            return{
-                ...state,
-                filtered: action.payload,
-            }
-        case 'ADD_FAVORITE':
-            return{
-                ...state,
-                favorites: [...state.favorites, action.payload]
-            }
-        case 'REMOVE_FAVORITE':
-            return{
-                ...state,
-                favorites: state.favorites.filter(f => f.id !== action.payload)
-            }
-        case 'ADD_CART':
-            return{
-                ...state,
-                cart: [...state.cart, action.payload]
-            }
-        case 'REMOVE_CART':
-            return{
-                ...state,
-                cart: state.cart.filter(c => c.id !== action.payload)
-            }
-        case 'TOGGLE_DARK_MODE':
-                return {
-                  ...state,
-                  darkMode: !state.darkMode
-                };
-        // case 'SET_SEARCH_TERM':
-        //     return {
-        //         ...state,
-        //         searchTerm: action.searchTerm
-        //     }
-        // case 'SET_SEARCH_RESULTS':
-        //     return {
-        //         ...state,
-        //         searchResults: action.results,
-        //     }
-        case 'GET_SEARCH_RESULTS':
-            return {
-                ...state,
-                results: action.payload,
-            }
-        case 'GET_RESULTS':
-            return {
-                ...state,
-                results2: action.payload,
-            }
-        case 'COMPLETE_PROFILE':
-            return {
-                ...state,
-                currentUser: action.payload,
-            }
-        case 'GET_REVIEWS_BY':
-            return {
-                ...state,
-                reviews: action.payload
-            }
-        case 'STATUS_REGISTER':
-            return {
-                ...state,
-                tokenAccepted: action.payload
-            }
-        case 'GET_SEARCH_TERM':
-            return{
-                ...state,
-                searchTerm: action.payload,
-            }
-        default:
-            return {...state}
+            return "Ordered";
+          });
+          return {
+            ...state,
+            productsByCategory: orderedProductsByPrice,
+          };
+      // case "ORDER_BY_PRICE":
+      //   return {
+      //     ...state,
+      //     filtered: action.payload,
+      //   };
+      case "ADD_FAVORITE":
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload],
+        };
+      case "REMOVE_FAVORITE":
+        return {
+          ...state,
+          favorites: state.favorites.filter((f) => f.id !== action.payload),
+        };
+      case "ADD_CART":
+        return {
+          ...state,
+          cart: [...state.cart, action.payload],
+        };
+      case "REMOVE_CART":
+        return {
+          ...state,
+          cart: state.cart.filter((c) => c.id !== action.payload),
+        };
+      case "TOGGLE_DARK_MODE":
+        return {
+          ...state,
+          darkMode: !state.darkMode,
+        };
+      // case 'SET_SEARCH_TERM':
+      //     return {
+      //         ...state,
+      //         searchTerm: action.searchTerm
+      //     }
+      // case 'SET_SEARCH_RESULTS':
+      //     return {
+      //         ...state,
+      //         searchResults: action.results,
+      //     }
+      case "GET_SEARCH_RESULTS":
+        return {
+          ...state,
+          results: action.payload,
+        };
+      case "GET_LOGGED_USER": {
+        return {
+          ...state,
+        };
+      }
+      case "SET_LOGGED_USER":
+        return {
+          ...state,
+          loggedUser: action.payload,
+        };
+      case "GET_REVIEWS_BY":
+        return {
+          ...state,
+          reviews: action.payload,
+        };
+      default:
+        return { ...state };
     }
-}
+  }
+  
