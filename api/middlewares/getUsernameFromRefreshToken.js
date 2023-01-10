06737e4616a8d435f7c68ef3c2567c4013a54c81
Alberto Martinez <srbetito85@gmail.com>
2023-01-10 03:32:34 -0300

    asd

--- api/middlewares/getUsernameFromRefreshToken.js
+++ api/middlewares/getUsernameFromRefreshToken.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
 function getUsernameFromRefreshToken(req, res, next) {
   const cookies = req.cookies?.jwt;
+  console.log("JWT", cookies);
   jwt.verify(cookies, process.env.REFRESH_TOKEN_SECRET, (err, decode) => {
     if (err) {
       req.username = null;
     } else {
       req.username = decode.username;
-      return res.sendStatus(408);
     }
   });
   console.log(req.username);
