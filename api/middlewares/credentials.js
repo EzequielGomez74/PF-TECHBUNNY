610feefba3d095dfbe2b7e553e58e2872b4dd32a
Alberto Martinez <srbetito85@gmail.com>
2023-01-10 01:38:50 -0300

    asd

--- api/middlewares/credentials.js
+++ api/middlewares/credentials.js
@@ -3,7 +3,7 @@ const allowedOrigins = require("../config/allowOrigins");
 function credentials(req, res, next) {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", "true");
   }
   next();
 }
