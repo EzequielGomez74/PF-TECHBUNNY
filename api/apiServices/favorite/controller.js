78b04e4c4454c79044e2676b555fbdb19b6596c1
Gerlini <gerfer97.gf@gmail.com>
2023-01-12 12:17:31 -0300

    tabla de favoritos mas clean

--- api/apiServices/favorite/controller.js
+++ api/apiServices/favorite/controller.js
@@ -1,26 +1,33 @@
-const { Favorite, Product } = require("../../services/db/db.js");
+const { Favorite , Product } = require("../../services/db/db.js");
 
 async function getAllFavorite(user_id) {
   try {
-    let favorites = await Favorite.findAll({ where: { user_id } });
-    return favorites;
-  } catch (error) {
+    let favorites = await Favorite.findAll({ where: { user_id }});
+    const result = [];
+    for (let i = 0; i < favorites.length; i++) {
+        const productData = await Product.findOne({where:{product_id: favorites[i].product_id}})
+        const favorite = {
+            ...favorites[i].dataValues,
+            hola: "hola front, todo bien?",
+            price: productData.dataValues.price,
+            name: productData.dataValues.name,
+            image: productData.dataValues.image,
+            stock: productData.dataValues.stock,
+            brand: productData.dataValues.brand,
+        };
+        result.push(favorite);
+    }
+    return result;
+} catch (error) {
     throw new Error(error.message);
-  }
-}
+}}
 
 async function createFavorite(body) {
   try {
     const { product_id, user_id} = body;
     const existe = await Favorite.findOne({ where: { product_id, user_id }});
     if (!existe) {
-      const productData = await Product.findOne({where:{product_id}})
       await Favorite.create({
-        price: productData.dataValues.price, 
-        name: productData.dataValues.name, 
-        image: productData.dataValues.image, 
-        stock: productData.dataValues.stock,
-        brand: productData.dataValues.brand,
         product_id, 
         user_id});
 
