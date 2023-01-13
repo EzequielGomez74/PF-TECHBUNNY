7037737086cfdd26d3a63f3354080312e124ab87
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-01-12 22:39:28 -0300

    Rama Developer Funcionando

--- api/apiServices/cart/model.js
+++ api/apiServices/cart/model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       price: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.FLOAT,
         allowNull: false,
       }
     },
