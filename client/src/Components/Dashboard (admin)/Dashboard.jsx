3438724d9abc79d2ec05b77816626681c1587206
abibreno <jenniferabigailbreno@gmail.com>
2023-01-17 19:58:39 -0300

    cambios front

---
+++ client/src/Components/Dashboard (admin)/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Backdrop from '../Toolbar/Backdrop'
+import Sidebar from '../Toolbar/Sidebar'
+import Toolbar from '../Toolbar/Toolbar'
+
+function Dashboard() {
+
+  const [sidebar, setSidebar] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebar((prevState)=> !prevState)
+  }
+
+  return (
+    <div>
+        <Toolbar openSidebar={toggleSidebar} />
+        <Backdrop SideBar={sidebar} closeSidebar={toggleSidebar} />
+        <Sidebar SideBar={sidebar} />
+    </div>
+  )
+}
+
+export default Dashboard
