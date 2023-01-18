f9e2306b03bb6206024a79d193decfdf21b32c72
Gerlini <gerfer97.gf@gmail.com>
2023-01-18 17:33:38 -0300

    cosas

--- api/apiServices/favorite/controller.js
+++ api/apiServices/favorite/controller.js
@@ -5,7 +5,9 @@ async function getAllFavorite(user_id) {
     let favorites = await Favorite.findAll({ where: { user_id } });
     const result = [];
     for (let i = 0; i < favorites.length; i++) {
-      const productData = await Product.findOne({ where: { product_id: favorites[i].product_id } })
+      const productData = await Product.findOne({
+        where: { product_id: favorites[i].product_id },
+      });
       const favorite = {
         ...favorites[i].dataValues,
         price: productData.dataValues.price,
@@ -24,22 +26,17 @@ async function getAllFavorite(user_id) {
 
 async function createFavorite(body) {
   try {
-    const { product_id, user_id} = body;
-    const existe = await Favorite.findOne({ where: { product_id, user_id }});
+    const { product_id, user_id } = body;
+    const existe = await Favorite.findOne({ where: { product_id, user_id } });
     if (!existe) {
-      const productData = await Product.findOne({where:{product_id}})
       await Favorite.create({
-        price: productData.dataValues.price, 
-        name: productData.dataValues.name, 
-        image: productData.dataValues.image, 
-        stock: productData.dataValues.stock,
-        brand: productData.dataValues.brand,
-        product_id, 
-        user_id});
+        product_id,
+        user_id,
+      });
 
       return "Producto agregado a favoritos!";
     } else {
-      await Favorite.destroy({ where: {product_id, user_id}});
+      await Favorite.destroy({ where: { product_id, user_id } });
       return "Producto eliminado a favoritos!";
     }
   } catch (error) {
