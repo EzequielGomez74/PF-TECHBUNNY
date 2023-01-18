1c55dde49dfccd774a660c75657c696100039ac8
abibreno <jenniferabigailbreno@gmail.com>
2023-01-18 17:36:07 -0300

    Estilos Recover

--- client/src/Components/Recover/Recover.jsx
+++ client/src/Components/Recover/Recover.jsx
@@ -3,6 +3,8 @@ import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 import axios from "axios";
 import img from "../../Photos/conejotriste.png";
+import s from "../Recover/Recover.module.css";
+import { useSelector } from "react-redux";
 
 function Recover() {
   const [email, setEmail] = useState("");
@@ -13,28 +15,37 @@ function Recover() {
     await axios.put("/enter/recover", { email });
   };
 
+  const dm = useSelector(state => state.darkMode);
+
   return (
     <div>
       <NavBar />
-      <div className="recoverContainer">
-      <div className="imgContainer">
-            <div className="imgBunny">
-              <img src={img} alt="bunny login"  />
+      <div className={dm ? s.dmrecoverContainer : s.recoverContainer}>
+        <form>
+          <div className={dm ? s.dmdivForm : s.divForm}>
+            <h3>¿Te olvidaste tu contraseña?</h3>
+            <div className={dm ? s.dmimgBunny : s.imgBunny}>
+            <img src={img} alt="bunny login" />
             </div>
+            <h5>
+              ¡No te preocupes! Escribe el email de tu cuenta{" "}
+              <strong>TECHBUNNY</strong> <br/> y recibe las instrucciones para
+              recuperarla.{" "}
+            </h5>
+            <div className={dm? s.dmdivInput : s.divInput}>
+            <input
+              type="text"
+              value={email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <div className={dm? s.dmdivBtnRecover : s.divBtnRecover}>
+            <button className={dm? s.dmbtnRecover : s.btnRecover}onClick={handleSendEmail}><p>Recuperar contraseña</p></button>
             </div>
-        <h3>¿Te olvidaste tu contraseña?</h3>
-        <h4>
-          ¡No te preocupes! Escribe el email de tu cuenta{" "}
-          <strong>TECHBUNNY</strong> y recibe las instrucciones para
-          recuperarla.{" "}
-        </h4>
-        <input
-          type="text"
-          value={email}
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <button onClick={handleSendEmail}>Recuperar contraseña</button>
+            
+            </div>
+          </div>
+        </form>
       </div>
       <Footer />
     </div>
